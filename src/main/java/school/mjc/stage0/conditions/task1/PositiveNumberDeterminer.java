a67becc6d8a0c4573ee7f79a661280482172f6f4
Aizhan-15 <aizhanurman@gmail.com>
2022-09-20 17:20:33 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        int number = 3;
+        if (number > 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
